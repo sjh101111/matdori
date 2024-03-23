d3cdd5d32872e9930da7e74e0b24c01e151ba8c7
taegyoo1104 <taegyoo1104@gmail.com>
2024-03-24 00:46:26 +0900

    Initial commit

---
+++ src/main/java/com/estsoft13/matdori/MatdoriApplication.java
@@ -0,0 +1,13 @@
+package com.estsoft13.matdori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatdoriApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatdoriApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/estsoft13/matdori/MatdoriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estsoft13.matdori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatdoriApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
