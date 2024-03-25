636f5a6ef045ec6fe4b41c25828e6fdc48c8e058
chc <311mikecho@gmail.com>
2024-03-25 11:46:18 +0900

    유저 테이블명 수정

--- src/main/java/com/estsoft13/matdori/domain/User.java
+++ src/main/java/com/estsoft13/matdori/domain/User.java
@@ -3,7 +3,7 @@ package com.estsoft13.matdori.domain;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "User")
+@Table(name = "Users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
