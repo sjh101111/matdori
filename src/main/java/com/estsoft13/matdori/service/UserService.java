c539e5a13f479e80d5109ac76eba94bfc6cc0927
chc <311mikecho@gmail.com>
2024-03-29 11:33:11 +0900

    암호화 및 유저 정보

--- src/main/java/com/estsoft13/matdori/service/UserService.java
+++ src/main/java/com/estsoft13/matdori/service/UserService.java
@@ -3,25 +3,28 @@ package com.estsoft13.matdori.service;
 import com.estsoft13.matdori.domain.User;
 import com.estsoft13.matdori.dto.UserDto;
 import com.estsoft13.matdori.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-
+@RequiredArgsConstructor
 @Service
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
 
+    private final UserRepository userRepository;
+
+    private final BCryptPasswordEncoder encoder;
     public void saveUser(UserDto userDto){
         User user = new User();
         user.setUsername(userDto.getUsername());
         user.setEmail(userDto.getEmail());
-        user.setPassword(userDto.getPassword());
+        user.setPassword(encoder.encode(userDto.getPassword()));
         userRepository.save(user);
     }
     public User findByEmail(String email) {
-        return userRepository.findByEmail(email);
+        return userRepository.findByEmail(email).
+                orElseThrow(() -> new IllegalArgumentException("User not found: " + email));
     }
 
     public String findPasswordByUsernameAndEmail(String username, String email){
@@ -31,4 +34,3 @@ public class UserService {
     }
 
 }
-
